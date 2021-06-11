9d85cdbc7b5cf9e3d9c4680ffb5ae4750c102872
unknown <ahmedmo@stud.cu.edu.eg>
2021-06-11 19:12:00 +0200

    appServices Package

---
+++ appServices/Requests.java
@@ -0,0 +1,24 @@
+package appServices;
+import usersPackage.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+
+public class Requests {
+
+	Player pp = new Player("dsd", "dsd", "dsd", "dsd", "dsd"); 
+
+	private HashMap<Map.Entry<Integer, Player>, Map.Entry<Playground, RequestStatus>> playGReq;
+	
+	public Requests()
+	{
+		playGReq = new HashMap<>();
+	}
+	
+	public String rr;
+	public void myBooks() {}
+	
+}
